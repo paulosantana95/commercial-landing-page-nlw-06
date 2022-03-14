11f73ef20de17079b098f5b3970159e868d3e861
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-13 21:58:38 -0300

    construction of new sections, added javascript to manipulate the navigate menu, added scroll effects and transition effects.

---
+++ main.js
@@ -0,0 +1,32 @@
+//menu em forma de toggle
+
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for (const element of toggle) {
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show');
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+    link.addEventListener('click', function(){
+        nav.classList.remove('show');
+    })
+}
+
+
+// mudar o header da página quando der scroll
+
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+})
